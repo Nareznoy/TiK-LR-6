0b06981ade17a31ec28fff9b11cfe588e3ea64a8
Денис Северин <deniseverin@mail.ru>
2021-05-13 17:31:39 +0300

    Добавьте файлы проекта.

---
+++ TiK LR №6/CRC16.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiK_LR__6
+{
+    class CRC16
+    {
+        const ushort poly = 0b111001;
+        ushort[] table = new ushort[256];
+
+        /// <summary>
+        /// Вычислить контрольную сумму
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public ushort ComputeChecksum(byte[] bytes)
+        {
+            ushort crc = 0xFFFF;
+
+            foreach (var it in bytes)
+            {
+                crc = (ushort)((crc << 8) ^ table[((crc >> 8) ^ (0xff & it))]);
+            }
+
+            return crc;
+        }
+
+        /// <summary>
+        /// Вычисление байтов контрольной суммы
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public byte[] ComputeChecksumBytes(byte[] bytes)
+        {
+            ushort crc = ComputeChecksum(bytes);
+            return BitConverter.GetBytes(crc);
+        }
+
+        /// <summary>
+        /// CRC 16 CITT
+        /// </summary>
+        /// <param name="initialValue"></param>
+        public CRC16()
+        {
+            ushort temp, a;
+            for (int i = 0; i < table.Length; ++i)
+            {
+                temp = 0;
+                a = (ushort)(i << 8);
+                for (int j = 0; j < 8; ++j)
+                {
+                    if (((temp ^ a) & 0x8000) != 0)
+                    {
+                        temp = (ushort)((temp << 1) ^ poly);
+                    }
+                    else
+                    {
+                        temp <<= 1;
+                    }
+                    a <<= 1;
+                }
+                table[i] = temp;
+            }
+        }
+    }
+}
---
+++ TiK LR №6/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TiK_LR__6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            CRC16 crc16 = new CRC16();
+
+            byte[] crcBytes = crc16.ComputeChecksumBytes(new byte[] { 1, 0, 0, 1, 1, 1, 0, 1 });
+            ushort crc = crc16.ComputeChecksum(new byte[] {1, 0, 0, 1, 1, 1, 0, 1});
+
+        }
+    }
+}
