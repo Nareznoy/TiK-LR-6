6f9cd22fcf09d6741941dafcac533cd708d9f44b
Денис Северин <deniseverin@mail.ru>
2021-05-18 01:38:34 +0300

    Added Comments

--- TiK LR №6/CRC.cs
+++ TiK LR №6/CRC.cs
@@ -4,27 +4,30 @@ namespace TiK_LR__6
 {
     static class CRC
     {
-        private static long _poly = 0b10010010101;
+        private static long _poly = 0b10010010101; // Генераторный полином: x^10 + x^7 + x^4 + x^2 + 1
         private static readonly int _polyLength = (int) Math.Log(_poly, 2) + 1;
 
         private static int _inputCodeLength;
 
         public static long CalculateCRC(long inputCode, bool isCheck = false)
         {
+            // Добавление в конце принятого сообщения нулей
             if(!isCheck)
                 inputCode <<= _polyLength - 1;
 
             _inputCodeLength = (int)Math.Log(inputCode, 2) + 1;
-            _poly <<= (_inputCodeLength - _polyLength);
+            _poly <<= (_inputCodeLength - _polyLength); // Сдвиг полинома до старшего бита принятого сообщения
 
             for (var i = 0; i < (_inputCodeLength - _polyLength); i++)
             {
+                // Когда полином сдвигается до текущего старшего бита, выполняется операция XOR
                 if ((inputCode & (1 << (_inputCodeLength - i - 1))) == (1 << (_inputCodeLength - i - 1)))
                 {
                     inputCode ^= _poly;
                 }
                 _poly >>= 1;
             }
+            // Если размер текущего остатка равен размеру полинома, то выполнить XOR еще один раз
             if ((int)Math.Log(inputCode, 2) + 1 == (int)Math.Log(_poly, 2) + 1)
             {
                 inputCode ^= _poly;
@@ -35,10 +38,10 @@ namespace TiK_LR__6
 
         public static bool CheckMessage(long message, long crc)
         {
-            message <<= _polyLength - 1;
-            long messagePlusCrcInt = message + crc;
+            message <<= _polyLength - 1; // Добавление в конец сообщения нулей
+            long messagePlusCrcInt = message + crc; // Прибавление к сообщению CRC кода
 
-            return CalculateCRC(messagePlusCrcInt, true) == 0;
+            return CalculateCRC(messagePlusCrcInt, true) == 0; // Равен ли CRC код нулю
         }
     }
 }
--- TiK LR №6/Form1.cs
+++ TiK LR №6/Form1.cs
@@ -7,10 +7,10 @@ namespace TiK_LR__6
 {
     public partial class Form1 : Form
     {
-        List<long> inputMessages = new List<long>();
-        List<long> CRCs = new List<long>();
-        List<bool> isCorrect = new List<bool>();
-        List<long> messagesForCheck = new List<long>();
+        readonly List<long> _inputMessages = new List<long>(); // Входные блоки данных
+        readonly List<long> _CRCs = new List<long>(); // CRC коды входных блоков данных
+        readonly List<bool> _isCorrect = new List<bool>(); // Содержит информацию о проверенных блоках данных
+        readonly List<long> _messagesForCheck = new List<long>(); // Принятые сообщения для проверки 
 
         public Form1()
         {
@@ -24,37 +24,36 @@ namespace TiK_LR__6
             string[] stringMessages = message16_textBox.Text.Split();
             foreach (string str in stringMessages)
             {
-                inputMessages.Add(Convert.ToInt64(str, fromBase));
-                message2_textBox.Text += Convert.ToString(inputMessages.Last(), toBase: 2) + " ";
-                CRCs.Add(CRC.CalculateCRC(inputMessages.Last()));
-                CalculatedCRC_textBox.Text += Convert.ToString(CRCs.Last(), toBase: 2) + " ";
-                InputCRC_textBox.Text += Convert.ToString(CRCs.Last(), toBase: 2) + " ";
+                // Конвертация входного блока данных
+                _inputMessages.Add(Convert.ToInt64(str, fromBase));
+                message2_textBox.Text += Convert.ToString(_inputMessages.Last(), toBase: 2) + " ";
+
+                // Получение CRC кода входного блока данных
+                _CRCs.Add(CRC.CalculateCRC(_inputMessages.Last())); 
+                CalculatedCRC_textBox.Text += Convert.ToString(_CRCs.Last(), toBase: 2) + " ";
+                InputCRC_textBox.Text += Convert.ToString(_CRCs.Last(), toBase: 2) + " ";
             }
         }
 
         private void CheckData_button_Click(object sender, EventArgs e)
         {
+            bool state = true; // Есть ли ошибка в принятом коде
+            string errorBlockNumber = ""; // Номер блока с ошибкой
+
             int fromBase = Convert.ToInt32(comboBox2.Text);
             string[] stringMessages = messageForCheck_textBox.Text.Split();
-
-            bool state = true;
-            string error = "";
-
-
             for (int i = 0; i < stringMessages.Length; i++)
             {
-                messagesForCheck.Add(Convert.ToInt64(stringMessages[i], fromBase));
+                _messagesForCheck.Add(Convert.ToInt64(stringMessages[i], fromBase));
 
-                isCorrect.Add(CRC.CheckMessage(messagesForCheck.Last(), CRCs[i]));
-                if (!isCorrect.Last())
-                {
-                    state = false;
-                    error += i + 1 + " ";
-                }
+                _isCorrect.Add(CRC.CheckMessage(_messagesForCheck.Last(), _CRCs[i])); // Проверка принятого сообщения
+
+                if (_isCorrect.Last()) continue; // Если сообщение без ошибок, то проверить следующее
+                state = false; 
+                errorBlockNumber += i + 1 + " "; 
             }
 
-
-            CheckResultLabel.Text = state ? "Данные приняты верно" : "Данные приняты с ошибкой в блоке: " + error;
+            CheckResultLabel.Text = state ? "Данные приняты верно" : "Данные приняты с ошибкой в блоке: " + errorBlockNumber;
         }
     }
 }
